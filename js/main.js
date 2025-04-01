dd04c59d8030f8016f4702a4e7aa4a7d5ed8cbed
Nicolapsq <nicolapasqua84@gmail.com>
2025-04-01 18:40:27 +0200

    Add-content

--- js/main.js
+++ js/main.js
@@ -1,3 +1,8 @@
+const prezzoBigliettoPer_km = 0.21;
+const scontoGiovani = 20 / 100
+const scontoAnziani = 40 / 100
+
+// Controllo KM
 let n_km = prompt("inserisci numero di km che vuoi percorrere");
 
 if (isNaN(n_km)){
@@ -11,32 +16,48 @@ else {
     alert("inserimento effettuato");
 }
 
-console.log(n_km);
+console.log(`${"kilometri"}, ${n_km}`);
 
+// Controllo Età
+let eta = prompt("inserisci la tua età");
 
-let età = prompt("inserisci la tua età");
-
-const prezzoBigliettoPer_km = 0.21;
-const scontoGiovani = 0.20
-const scontoAnziani = 0.40
-
-if (isNaN(età)){
+if (isNaN(eta)){
     alert("Devi inserire l'età in numero");
 }
-else if (età < 6){
+else if (eta < 6){
     alert("devi avere almeno 6 anni")
-}
-else if (età > 65){
-    let costo = alert("questo è il prezzo" + " " + (prezzoBigliettoPer_km * n_km) / scontoAnziani + "€");
-}
-else if (età < 18){
-    let costo = alert("questo è il prezzo" + " " + (prezzoBigliettoPer_km * n_km) / scontoGiovani + "€");
-}
-else {
-    alert("inserimento età effettuata");
+} else {
+
+    // Calcolo biglietto
+    if (eta >= 65){
+        // - Calcolo il prezzo del biglietto con lo sconto
+        let prezzoSenzaSconto = prezzoBigliettoPer_km * n_km;
+        console.log(`${"prezzo senza sconto"}, ${prezzoSenzaSconto}`);
+    
+        //   - Calcolo lo sconto del biglietto
+        let prezzoFinale = prezzoSenzaSconto - (prezzoSenzaSconto * scontoAnziani);
+        console.log(`${"prezzo finale con sconto anziani"}, ${prezzoFinale.toFixed(2)}`);
+        //   - Sottraggo lo sconto al prezzo del biglietto senza sconto
+        // Stampo il prezzo finale del biglietto
+        alert("questo è il prezzo" + " " + (prezzoFinale) + "€");
+    }
+    else if (eta < 18){
+         // - Calcolo il prezzo del biglietto con lo sconto
+         let prezzoSenzaSconto = prezzoBigliettoPer_km * n_km;
+        console.log(`${"prezzo senza sconto"}, ${prezzoSenzaSconto}`);
+         
+         //   - Calcolo lo sconto del biglietto
+         let prezzoFinale = (prezzoSenzaSconto - (prezzoSenzaSconto * scontoGiovani));
+        console.log(`${"prezzo finale con sconto giovani"}, ${prezzoFinale.toFixed(2)}`);
+    }
+
+    else {
+        alert("inserimento età effettuata");
+    }
+    
+    console.log(`${"la sua età è di"}, ${eta}`);
 }
 
-console.log(età);
 
 
 
